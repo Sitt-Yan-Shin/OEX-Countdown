31cf372b5f310016c09070111b014f2b09129760
Sitt-Yan-Shin <125720610+Sitt-Yan-Shin@users.noreply.github.com>
2024-03-31 12:20:24 +0630

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,40 @@ const days = document.querySelector(".Time--days");
 const hours = document.querySelector(".Time--hours");
 const minutes = document.querySelector(".Time--minutes");
 const seconds = document.querySelector(".Time--seconds");
+const toMoon = document.querySelector(".to-moon");
+const h2 = document.getElementsByTagName("h2")[0]; // Assuming there's only one h2 element
+const boxes = document.querySelectorAll(".box"); // Assuming there's only one element with the class 'box'
+const text = document.querySelector(".text");
 
-const now = new Date().getTime();
-const date = new Date("2024-04-13")
+const date = new Date("March 31, 2024 24:00"); // 9:00 PM
 
-function UpdateTime(){
+const myInterval = setInterval(UpdateTime, 1000);
+
+function UpdateTime() {
   const now = new Date();
-  const diff = date-now;
-  const d = Math.floor(diff/1000/60/60/24);
-  const h = Math.floor((diff/1000/60/60)%24);
-  const m = Math.floor((diff/1000/60)%60);
-  const s = Math.floor((diff/1000)%60);
-  const ms = Math.floor(diff%1000);
 
-  days.innerHTML=d<10?"0"+d:d;
-  hours.innerHTML=h<10?"0"+h:h;
-  minutes.innerHTML=m<10?"0"+m:m;
-  seconds.innerHTML=s<10?"0"+s:s;
+  const diff = date - now;
+  const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const m = Math.floor((diff / (1000 * 60)) % 60);
+  const s = Math.floor((diff / 1000) % 60);
+
+  days.innerHTML = d < 10 ? "0" + d : d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+
+  if (d == 0 && h == 0 && m == 0 && s == 0) {
+    clearInterval(myInterval);
+    boxes.forEach(box => {
+      box.classList.add("box2");
+      h2.classList.add("box2")
+      text.innerHTML = "To the Moon🚀"
+      text.classList.add("to-the-moon")
+    })
+    confetti({
+      particleCount: 300,
+      spread: 200
+    })
+  }
 }
-
-setInterval(UpdateTime, 1000);
